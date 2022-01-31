35a69b3596063f056742a9ebef6666b7c93c346a
Flancian <0@flancia.org>
2022-02-01 00:42:54 +0100

    Also reply in threads.

--- agora.py
+++ agora.py
@@ -8,10 +8,23 @@ import re
 
 THREAD = RelationType("io.element.thread")
 
+def inThread(evt):
+    try:
+        content = evt.content
+        relates = content._relates_to
+        if relates.rel_type==THREAD:
+            print(f"*** event was already in thread")
+            return True
+        return False
+    except:
+        return False
+    
+
 class AgoraPlugin(Plugin):
     @command.passive("\[\[(.+?)\]\]", multiple=True)
     async def handler(self, evt: MessageEvent, subs: List[Tuple[str, str]]) -> None:
         await evt.mark_read()
+        print(f"responding to event: {evt}")
         response = ""
         wikilinks = []  # List of all wikilinks given by user
         for _, link in subs:
@@ -30,8 +43,13 @@ class AgoraPlugin(Plugin):
         if wikilinks:
             wikilinks = f"\n".join(wikilinks)
             response += wikilinks
-            content = TextMessageEventContent(
-                    body=response, 
-                    msgtype=MessageType.NOTICE,
-                    relates_to=RelatesTo(rel_type=THREAD, event_id=evt.event_id))
-            await evt.respond(content, allow_html=True)  # Reply to user
+            if inThread(evt):
+                # already in a thread, can't start one :)
+                await evt.reply(response, allow_html=True)
+            else:
+                # start a thread with our reply.
+                content = TextMessageEventContent(
+                        body=response, 
+                        msgtype=MessageType.NOTICE,
+                        relates_to=RelatesTo(rel_type=THREAD, event_id=evt.event_id))
+                await evt.respond(content, allow_html=True)  # Reply to user
