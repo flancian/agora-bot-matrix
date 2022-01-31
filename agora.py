d679119e425a79951c69a233a006f3650c873492
Flancian <0@flancia.org>
2022-01-31 09:29:34 +0100

    Initial agorabot functionality.

--- agora.py
+++ agora.py
@@ -3,19 +3,26 @@ from typing import List, Tuple
 import urllib.parse
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
+import re
 
 
 class AgoraPlugin(Plugin):
-    @command.passive("\[\[(.+)\]\]", multiple=True)
+    @command.passive("\[\[(.+?)\]\]", multiple=True)
     async def handler(self, evt: MessageEvent, subs: List[Tuple[str, str]]) -> None:
         await evt.mark_read()
+        response = ""
         wikilinks = []  # List of all wikilinks given by user
         for _, link in subs:
-            link = "https://anagora.org/{}".format(urllib.parse.quote(link))
+            if re.match('[0-9a-zA-Z -]+$', link):
+                # prefer slugging simple links
+                link = "https://anagora.org/{}".format(link.replace(' ', '-'))
+            else:
+                # urlencode otherwise
+                link = "https://anagora.org/{}".format(urllib.parse.quote(link))
 
             wikilinks.append(link)
 
         if wikilinks:
             wikilinks = f"\n".join(wikilinks)
-            response = wikilinks
+            response += wikilinks
             await evt.reply(response, allow_html=True)  # Reply to user
