ce4f617a78ca9491d37ae87e97c05ab16d74a9fb
Flancian <0@flancia.org>
2022-02-27 01:23:14 +0100

    Add simple Agora writes :)

--- agora.py
+++ agora.py
@@ -4,8 +4,15 @@ import urllib.parse
 from maubot import Plugin, MessageEvent
 from mautrix.types import RelationType, TextMessageEventContent, RelatesTo, MessageType
 from maubot.handlers import command
+import datetime
+import os
 import re
 
+AGORA_BOT_ID="anagora@matrix.org"
+AGORA_URL=f"https://anagora.org"
+MATRIX_URL=f"https://develop.element.io"
+AGORA_ROOT=os.path.expanduser("~/agora")
+OUTPUT_DIR=f"{AGORA_ROOT}/stream/{AGORA_BOT_ID}"
 THREAD = RelationType("io.element.thread")
 
 def inThread(evt):
@@ -19,6 +26,36 @@ def inThread(evt):
     except:
         return False
     
+def log_evt(evt, node):
+
+    try:
+        os.mkdir(OUTPUT_DIR)
+    except FileExistsError:
+        pass
+
+    # unsure if it's OK inlining, perhaps fine in this case as each room does explicit setup?
+    # msg = evt.body
+
+    # this was shamelessly copy/pasted and adapted from [[agora bridge]], mastodon bot.
+    if ('/' in node):
+        # for now, dump only to the last path fragment -- this yields the right behaviour in e.g. [[go/cat-tournament]]
+        node = os.path.split(node)[-1]
+
+    filename = os.path.join(OUTPUT_DIR, node + '.md')
+    print(f"logging {evt} to file {filename} mapping to {node}.")
+
+    # hack hack -- this should be enabled/disabled/configured in the maubot admin interface somehow?
+    try:
+        with open(filename, 'a') as note:
+            username = evt.sender
+            # /1000 needed to reduce 13 -> 10 digits
+            dt = datetime.datetime.fromtimestamp(int(evt.timestamp/1000))
+            link = f'{MATRIX_URL}/#/room/{evt.room_id}/{evt.event_id}'
+            # note.write(f"- [[{username}]] at {dt}: {link}\n  - ```{msg}```")
+            note.write(f"- [[{dt}]]\n  - [[{username}]] {link}\n")
+    except Exception as e:
+        print(f"Couldn't save link to message, exception: {e}.")
+
 
 class AgoraPlugin(Plugin):
     @command.passive("\[\[(.+?)\]\]", multiple=True)
@@ -41,8 +78,7 @@ class AgoraPlugin(Plugin):
             wikilinks.append(link)
 
         if wikilinks:
-            wikilinks = f"\n".join(wikilinks)
-            response += wikilinks
+            response = f"\n".join(wikilinks)
             if inThread(evt):
                 # already in a thread, can't start one :)
                 await evt.reply(response, allow_html=True)
@@ -53,3 +89,7 @@ class AgoraPlugin(Plugin):
                         msgtype=MessageType.NOTICE,
                         relates_to=RelatesTo(rel_type=THREAD, event_id=evt.event_id))
                 await evt.respond(content, allow_html=True)  # Reply to user
+
+            # try to save a link to the message in the Agora.
+            for wikilink in wikilinks:
+                log_evt(evt, wikilink)
